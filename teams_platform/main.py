39bf938169834f571421a7ddd1c6d67b6c10d5f1
NORXND <szymon.piotr.grzegorzewski@gmail.com>
2020-11-18 09:57:38 +0100

    Ważna poprawka komend na Teams. Robie też test na Heroku!

--- teams_platform/main.py
+++ teams_platform/main.py
@@ -18,7 +18,7 @@ async def api_handler(context):
 
 # Rozprowadza event do odpowiedniej funkcji.
 async def message_handler(context):
-    cmd = " ".join(list(context.activity.text.split(" "))[1])
+    cmd = "".join(list(context.activity.text.split(" "))[1])
     ctx = context
     arg = " ".join(list(context.activity.text.split(" "))[2:])
     # Szuka komendy, niestety nie da się chyba inaczej tego zrobić...
