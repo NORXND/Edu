18fe9febd2e03ee2b2e907c11ee58f950f2c067d
NORXND <szymon.piotr.grzegorzewski@gmail.com>
2020-11-18 13:05:36 +0100

    Dodanie indykatoru pisania

--- teams_platform/main.py
+++ teams_platform/main.py
@@ -2,6 +2,7 @@ from botbuilder.core import (
     BotFrameworkAdapter,
     BotFrameworkAdapterSettings,
 )
+from botbuilder.schema import Activity, ActivityTypes
 
 import teams_platform.commands.wiki as cmd_wiki
 from storage import SECRET
@@ -19,6 +20,7 @@ async def api_handler(context):
 
 # Rozprowadza event do odpowiedniej funkcji.
 async def message_handler(context):
+    await context.send_activity(Activity(type=ActivityTypes.typing))
     cmd = "".join(list(context.activity.text.split(" "))[1])
     ctx = context
     arg = " ".join(list(context.activity.text.split(" "))[2:])
